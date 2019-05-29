74c3ec1adeee929d7ee52d20bdc02d259c13ca48
Vince250598 <vincentgagnon53@gmail.com>
2019-05-29 13:09:47 -0400

    Added Artefact verification with sourceCode in createType method

--- src/main/java/swarm/server/repositories/ArtefactRepository.java
+++ src/main/java/swarm/server/repositories/ArtefactRepository.java
@@ -1,9 +1,14 @@
 package swarm.server.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import swarm.server.domains.Artefact;
 
 public interface ArtefactRepository extends JpaRepository<Artefact, Long> {
 
+	Artefact findBySourceCode(String sourceCode); //maybe change for hashcode
+	
 }
--- src/main/java/swarm/server/services/TypeService.java
+++ src/main/java/swarm/server/services/TypeService.java
@@ -1,5 +1,7 @@
 package swarm.server.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import swarm.server.domains.Artefact;
 import swarm.server.domains.Namespace;
 import swarm.server.domains.Session;
 import swarm.server.domains.Type;
+import swarm.server.repositories.ArtefactRepository;
 import swarm.server.repositories.TypeRepository;
 
 @Service
@@ -18,18 +21,36 @@ import swarm.server.repositories.TypeRepository;
 public class TypeService {
 
 	private final TypeRepository typeRepository;
+	private final ArtefactRepository artefactRepository;
 	
 	@Autowired
-	public TypeService(TypeRepository typeRepository) {
+	public TypeService(TypeRepository typeRepository, ArtefactRepository artefactRepository) {
 		this.typeRepository = typeRepository;
+		this.artefactRepository = artefactRepository;
 	}
 	
 	@GraphQLMutation
 	public Type createType(@GraphQLArgument(name = "namespace") Namespace namespace, @GraphQLArgument(name = "session") Session session, 
 			@GraphQLArgument(name = "fullName") String fullName, @GraphQLArgument(name = "fullPath") String fullPath, 
-			@GraphQLArgument(name = "name") String name, @GraphQLArgument(name = "artefact") Artefact artefact) {
+			@GraphQLArgument(name = "name") String name, @GraphQLArgument(name = "source") String sourceCode) {
+
+		Artefact artefact = artefactRepository.findBySourceCode(sourceCode);
+		
+		if(artefact == null) {
+			artefact = new Artefact(sourceCode, 1L); //change for incremental version?
+			artefactRepository.save(artefact);
+		}
+
+
 		return typeRepository.save(new Type(namespace, session, fullName, fullPath, name, artefact));
 	}
+	
+	/*@GraphQLMutation
+	public Type createType(@GraphQLArgument(name = "namespace") Namespace namespace, @GraphQLArgument(name = "session") Session session, 
+			@GraphQLArgument(name = "fullName") String fullName, @GraphQLArgument(name = "fullPath") String fullPath, 
+			@GraphQLArgument(name = "name") String name, @GraphQLArgument(name = "artefact") Artefact artefact) {
+		return typeRepository.save(new Type(namespace, session, fullName, fullPath, name, artefact));
+	}*/
 	
 	@GraphQLQuery
 	public Iterable<Type> typesBySessionId(@GraphQLArgument(name = "sessionId") Long sessionId){
