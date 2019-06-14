3ef4d733f839d3483497cd88ea1948fdbc5d73c6
Vince250598 <vincentgagnon53@gmail.com>
2019-06-14 18:28:03 -0400

    Added GraphQL query for {health}

---
+++ src/main/java/swarm/server/services/GeneralService.java
@@ -0,0 +1,17 @@
+package swarm.server.services;
+
+import org.springframework.stereotype.Service;
+
+import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
+
+@Service
+@GraphQLApi
+public class GeneralService {
+	
+	@GraphQLQuery(name = "health")
+	public String health() {
+		return "UP";
+	}
+
+}
