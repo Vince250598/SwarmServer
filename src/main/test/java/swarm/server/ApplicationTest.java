95d49e0c841cdd10bc11c30f886bc51f055db312
Vince250598 <vincentgagnon53@gmail.com>
2019-07-04 12:51:22 -0400

    Added Tests

---
+++ src/main/test/java/swarm/server/ApplicationTest.java
@@ -0,0 +1,12 @@
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    public void contextLoads() throws Exception {
+
+    }
+}
